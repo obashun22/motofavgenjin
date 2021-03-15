7336abb783255f294e076abd551d73e15ae4ae97
obashun22 <obashun22@yahoo.co.jp>
2021-03-16 01:27:33 +0900

    [fix] かわいいミスを修正

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth)
 
-api.update_status("Hello World via Tweepy!!/nby Heroku")
+api.update_status("Hello World via Tweepy!!\nby Heroku")
 
 # public_tweets = api.home_timeline()
 # for tweet in public_tweets:
