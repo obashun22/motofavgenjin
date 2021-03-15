d0373230d33ad1809b1c21e51894b46c375faf9d
obashun22 <obashun22@yahoo.co.jp>
2021-03-16 01:57:27 +0900

    [fix] ACCESS_TOKENに修正

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import tweepy
 
 consumer_key = os.environ['CONSUMER_KEY']
 consumer_secret = os.environ['CONSUMER_SECRET']
-access_token = os.environ['ACCESS_TOKEN_KEY']
+access_token = os.environ['ACCESS_TOKEN']
 access_token_secret = os.environ['ACCESS_TOKEN_SECRET']
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
